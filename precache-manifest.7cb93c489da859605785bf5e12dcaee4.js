b16da38224791d930d5d77d82774858885865d66
TEMP$ git config --global user.name TEMP <email@example.com>
2019-06-03 20:57:28 +0800

    commit 1.0.1

---
+++ precache-manifest.7cb93c489da859605785bf5e12dcaee4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5efb644b561f032e208adca2cebd22",
+    "url": "/ArkToolDemo/index.html"
+  },
+  {
+    "revision": "ff867d8b1ecf54729862",
+    "url": "/ArkToolDemo/static/css/2.19a4df75.chunk.css"
+  },
+  {
+    "revision": "d7bde8b4cb890d528704",
+    "url": "/ArkToolDemo/static/css/main.dd7a5864.chunk.css"
+  },
+  {
+    "revision": "ff867d8b1ecf54729862",
+    "url": "/ArkToolDemo/static/js/2.08ce59a8.chunk.js"
+  },
+  {
+    "revision": "d7bde8b4cb890d528704",
+    "url": "/ArkToolDemo/static/js/main.44161b6c.chunk.js"
+  },
+  {
+    "revision": "c0a0ae9c8502538fd194",
+    "url": "/ArkToolDemo/static/js/runtime~main.eadfb148.js"
+  },
+  {
+    "revision": "1150cc1442e6668cb949a74bfba27a69",
+    "url": "/ArkToolDemo/static/media/0.1150cc14.png"
+  },
+  {
+    "revision": "378475202d6cf6f5730815856392c35e",
+    "url": "/ArkToolDemo/static/media/0.37847520.png"
+  },
+  {
+    "revision": "9b3f2b8ccc2334d506c335c3edfbf8c9",
+    "url": "/ArkToolDemo/static/media/1.9b3f2b8c.png"
+  },
+  {
+    "revision": "5206203a924240e53bbb0809082ecb90",
+    "url": "/ArkToolDemo/static/media/10.5206203a.png"
+  },
+  {
+    "revision": "3af842d5697825805f00ad8633e0a224",
+    "url": "/ArkToolDemo/static/media/2.3af842d5.png"
+  },
+  {
+    "revision": "07f7da11f0c16fcb78b194229f5b4612",
+    "url": "/ArkToolDemo/static/media/3.07f7da11.png"
+  },
+  {
+    "revision": "80b08cf74a87082a7b4d198faa269bf0",
+    "url": "/ArkToolDemo/static/media/4.80b08cf7.png"
+  },
+  {
+    "revision": "84586f564fdde9907a4b3ee3722713a3",
+    "url": "/ArkToolDemo/static/media/8.84586f56.png"
+  },
+  {
+    "revision": "1f93e2765072cdb9296b18a99d048c4c",
+    "url": "/ArkToolDemo/static/media/9.1f93e276.png"
+  },
+  {
+    "revision": "c1ba329ad78ace68e29695a252288fff",
+    "url": "/ArkToolDemo/static/media/LV-text.c1ba329a.svg"
+  },
+  {
+    "revision": "1d9064f322704825ea9533571648832d",
+    "url": "/ArkToolDemo/static/media/bg_clear.1d9064f3.png"
+  },
+  {
+    "revision": "ac1256d37831fedcbe381118fa543c42",
+    "url": "/ArkToolDemo/static/media/star.ac1256d3.svg"
+  }
+]);
